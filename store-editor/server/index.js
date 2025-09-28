3bc8df86785f3adfc91c75b5520f03a468337ffb
Rx7QB9Lx3kn2wvY <Yralltheusernamestaken@gmail.com>
2025-09-27 18:07:31 -0700

    Improve graceful shutdown with async MongoDB close
    Refactored SIGINT handler to use async/await for closing the MongoDB connection. Added error handling to log and exit with code 1 if closing fails.

--- store-editor/server/index.js
+++ store-editor/server/index.js
@@ -513,9 +513,13 @@ app.post('/request-pickwalk', async (req, res) => {
 // -------------------- End of GTSP Routes --------------------
 
 // For graceful shutdown
-process.on('SIGINT', () => {
-  mongoose.connection.close(() => {
+process.on('SIGINT', async () => {
+  try {
+    await mongoose.connection.close();
     console.log('MongoDB connection closed');
     process.exit(0);
-  });
+  } catch (err) {
+    console.error('Error closing MongoDB connection:', err);
+    process.exit(1);
+  }
 });
